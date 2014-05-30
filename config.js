91b1a9c2daf026ea38a2632c19c8f9a4ed9ca351
hwclass <harderworking.class@gmail.com>
2014-05-30 11:15:06 +0300

    Mongo connection url taken into config file.

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 var constants = constants || {};
 
 var constants = {
+	mongo : {
+		url : 'mongodb://hwclass:123456@novus.modulusmongo.net:27017/Toxyh2is'
+	},
 	messages : {
 		error : {
 			anErrorOccured : 'An error occured. Please try again.'
